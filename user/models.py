753c337791c1163c06eb8da9cd8ce6418059807c
kojinsu <102194906+kojinsu@users.noreply.github.com>
2022-03-31 10:05:31 +0900

    고진수
    로그인 회원가입

---
+++ user/models.py
@@ -0,0 +1,20 @@
+from tabnanny import verbose
+from django.db import models
+
+class User(models.Model):
+
+    username = models.CharField(max_length=128, verbose_name = '이름')
+    birthday = models.IntegerField(default=0, verbose_name='생년월일')
+    phoneNum = models.IntegerField(default=0, verbose_name='휴대폰번호')
+    email = models.EmailField(max_length=128, verbose_name='이메일')
+    userId = models.CharField(max_length=128, verbose_name = '아이디')
+    password = models.CharField(max_length=128, verbose_name = '비밀번호')
+
+    class Meta:
+        db_table = 'movie_user'
+        verbose_name = '사용자'
+        verbose_name_plural = '사용자(들)'
+    
+    def __str__(self):
+        return f'{self.username}'
+
---
+++ user/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, redirect, HttpResponse
+from .models import User
+from django.contrib.auth.hashers import make_password, check_password
+
+def home(request):
+    userId = request.session.get('user')
+    if userId:
+        user = User.objects.get(pk=userId)
+        return render(request, "home.html")
+        # return HttpResponse(f'<h1>{user.username}님 반갑습니다</h1>')
+
+    return render(request, "home.html")
+
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        birthday = request.POST['birthday']
+        phoneNum = request.POST['phoneNum']
+        email = request.POST['email']
+        userId = request.POST['userId']
+        password = request.POST['password']
+        re_password = request.POST['re-password']
+
+        res_data = {}
+        if not (username and birthday and phoneNum and email and userId and password and re_password):  
+            res_data['error'] = '모든 값을 입력해야 합니다'
+        elif password != re_password:
+            res_data['password_error'] = '비밀번호가 다릅니다'
+        else:
+            user = User(
+                username = username,
+                birthday = birthday,
+                phoneNum = phoneNum,
+                email = email,
+                userId = userId,
+                password = make_password(password),
+            )
+        
+            user.save()
+
+        return render(request, 'register.html', res_data)
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        userId = request.POST.get('userId', None)
+        password = request.POST.get('password', None)
+
+        res_data = {}
+        if not(userId and password):
+            res_data['error'] = '모든 값을 입력해야 합니다.'
+        else:
+            user = User.objects.get(userId=userId)
+            if check_password(password, user.password):
+                request.session['user'] = user.id
+                return redirect('/')
+            else:
+                res_data['error'] = '비밀번호를 틀렸습니다.'
+
+        return render(request, 'login.html', res_data)
+
+def logout(request):
+    if request.session.get('user'):
+        del(request.session['user'])
+
+    return redirect('/')
