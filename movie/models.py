1f241720e833b9fcc5aaebe7a9ffc6649286e5b7
Leeholim <holim97@naver.com>
2022-04-05 17:13:26 +0900

    # 영화 모델들 수정
    이제 영화 모델들은 수정할일 없을것같습니다

--- movie/models.py
+++ movie/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
-class Movie(models.Model): # 영화 DB
-    movie_code = models.CharField(max_length=100, primary_key=True) # 영화코드
-    # 평점 ForignKey
-    movie_title = models.CharField(max_length=30) # 영화제목
-    movie_overview = models.TextField() # 줄거리
-    movie_director = models.CharField(max_length=100) # 감독
-    movie_poster = models.CharField(max_length=100) # 포스터
-    movie_company = models.CharField(max_length=50) # 제작사
-    movie_opendt = models.DateField() # 개봉일
+class Movie(models.Model):
+    movie_id = models.IntegerField() # 영화코드가 없는 관계로 id
+    title    = models.CharField(max_length=100) # 제목
+    released_date = models.CharField(max_length=50) # 개봉일
+    popularity = models.FloatField() # 인기
+    vote_avg = models.FloatField() # 평점
+    overview = models.TextField() # 줄거리
+    poster_path = models.CharField(max_length=150) # 포스터
+    genres = models.CharField(max_length=50) # 장르 (id값으로만 나와있어 어떻게할지 고민중)
+    actors = models.JSONField(null=True) # 배우들
+    director = models.CharField(max_length=100, null=True) # 감독 (필드밖에 있어 쓸수있을지 모르겠음) 제작사는 가져오는 API에 없어서 뺐습니다
 
     class Meta:
         db_table = 'movie'
 
+
     def __str__(self):
-        return f'{self.movie_title}'
+        return f'{self.title}'
