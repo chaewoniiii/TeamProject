bd8b480882a4e8908480c6a6e8b87e2cb0d99847
chaewoniiii <102212394+chaewoniiii@users.noreply.github.com>
2022-04-06 11:19:16 +0900

    영화편model, movieAdmin model
    영화편 model 추가
    movieAdmin -> 지역, 지점 model 추가
    ticket_book.html 수정
    ticket -> views.py 수정

--- movieAdmin/models.py
+++ movieAdmin/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Area_info(models.Model):
+    area = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'area_info'
+
+    def __str__(self):
+        return super().__str__()
+
+class Branch_office(models.Model):
+    branch_name = models.CharField(max_length=200)
+
+    area = models.ForeignKey('Area_info', on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'branch_office'
+
+    def _str__(self):
+        return super().__str__()
