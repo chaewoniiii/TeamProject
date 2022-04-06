a7abe2a41007819352cbaf5f0c88eec7fb2ada3b
Ureto <wjdwogud137@naver.com>
2022-04-06 09:19:52 +0900

    0406#1 migations 삭제 후 재설치
    DB가 꼬여서 초기화.

--- movie/admin.py
+++ movie/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Movie
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('movie_code', 'movie_title',)
+    list_display = (id, 'movie_id', 'title',)
 
 
 admin.site.register(Movie, MovieAdmin)
--- movie/models.py
+++ movie/models.py
@@ -1,15 +1,16 @@
+from email.policy import default
 from django.db import models
 
 class Movie(models.Model):
-    movie_id = models.IntegerField() # 영화코드가 없는 관계로 id
+    movie_id = models.IntegerField(default=0) # 영화코드가 없는 관계로 id
     title    = models.CharField(max_length=100) # 제목
     released_date = models.CharField(max_length=50) # 개봉일
-    popularity = models.FloatField() # 인기
-    vote_avg = models.FloatField() # 평점
+    popularity = models.FloatField(default=0) # 인기
+    vote_avg = models.FloatField(default=0) # 평점
     overview = models.TextField() # 줄거리
     poster_path = models.CharField(max_length=150) # 포스터
     genres = models.CharField(max_length=50) # 장르 (id값으로만 나와있어 어떻게할지 고민중)
-    actors = models.JSONField(null=True) # 배우들
+    actors = models.JSONField(default=[0,0] ,null=True) # 배우들
     director = models.CharField(max_length=100, null=True) # 감독 (필드밖에 있어 쓸수있을지 모르겠음) 제작사는 가져오는 API에 없어서 뺐습니다
 
     class Meta:
