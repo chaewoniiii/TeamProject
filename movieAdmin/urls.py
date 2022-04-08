4aa15b2c57bbc6b61d48a50d7b139194e99bba1c
Ureto <wjdwogud137@naver.com>
2022-04-08 13:03:49 +0900

    0408#2 관리자페이지 틀 구성
    관리자 페이지 틀 만 구성 url : movieAdmin
    접속방법 : 메인홈에서 admin 아이디 만든후 로그인 후  접속

--- movieAdmin/urls.py
+++ movieAdmin/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'Madmin'
+
 urlpatterns = [
     path('', views.movieAdminMain),
-    # path("movie_management/", views.movie_management, name="movie_management"),
+    path("movie_management/", views.movie_management, name="movie_management"),
 ]
--- movieAdmin/views.py
+++ movieAdmin/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import redirect, render
 from user.models import User
+from movie.models import Movie
 
 # Create your views here.
 def movieAdminMain(request):
     if not request.session.get('user'):
         return redirect('/user/login/')
+
     user_id = request.session.get('user')
     user = User.objects.get(pk=user_id)
     adminchk = str(user).startswith('admin')
+    
     if adminchk:
         return render(request, 'movie_adminMain.html')
     else: 
         return redirect('/')
 
-# def movie_management(request): # 영화 모델 추가하고 여기도 영화 DB를 똑같이 movie app 처럼 가져와서 씀
-#    moviedata = Movie.objects.all()
-#    return render(request, 'movie_management.html', {'moviedata': moviedata})
+def movie_management(request): # 영화 모델 추가하고 여기도 영화 DB를 똑같이 movie app 처럼 가져와서 씀
+   moviedata = Movie.objects.all()
+   return render(request, 'movie_management.html', {'moviedata': moviedata})
